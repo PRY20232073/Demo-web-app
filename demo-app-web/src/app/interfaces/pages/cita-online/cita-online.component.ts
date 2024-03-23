3d644f519a5ba33570589920e1f57f1126c31eca
Alonso Chirinos <achirinos@peqconsultores.com>
2024-03-23 00:43:19 -0500

    fix names

--- demo-app-web/src/app/interfaces/pages/cita-online/cita-online.component.ts
+++ demo-app-web/src/app/interfaces/pages/cita-online/cita-online.component.ts
@@ -24,7 +24,7 @@ export class CitaOnlineComponent {
   especialidad = new FormControl('');
   options: string[] = [
     'Cardiología',
-    'Dermatologíawo',
+    'Dermatología',
     'Obstetricia',
     'Urologia',
   ];
